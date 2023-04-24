abfd8b53ed767b9415530f30adedb01e0f9a2854
gaurav0116 <dhandhukiya218@gmail.com>
2023-04-24 19:21:09 +0530

    user search sort logic

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,34 @@ const { usersModel } = require("../model/users");
 router.get('/', async function (req, res, next) {
   try {
 
-    const usersList = await usersModel.aggregate([
+  
+    const pipeline =[];
+    let sortBy ="createdAt";
+    let sortOrder = -1;
+
+    // when search user
+    if (req.query.search) {
+      console.log("search =>", req.query.search);
+      pipeline.push({
+        $match: {
+          $or: [
+            { firstName: { $regex: req.query.search, $options: "i" } },
+            { lastName: { $regex: req.query.search, $options: "i" } },
+            { email: { $regex: req.query.search, $options: "i" } },
+            { fullName: { $regex: req.query.search, $options: "i" } } 
+          ]
+        },
+      });
+    }
+
+    // when sort user
+    if(req.query.sortOrder){
+      console.log("req.query =>", req.query);
+      sortOrder = Number(req.query.sortOrder);
+    }
+
+    // lookup 1
+    pipeline.push(
       {
         $lookup: {
           from: "savedposts",
@@ -14,7 +41,11 @@ router.get('/', async function (req, res, next) {
           foreignField: "savedBy",
           as: "savedPost"
         }
-      },
+      }
+    );
+
+    // lookup 2
+    pipeline.push(
       {
         $lookup: {
           from: "posts",
@@ -22,11 +53,16 @@ router.get('/', async function (req, res, next) {
           foreignField: "postBy",
           as: "createPost"
         }
-      },
+      }
+    );
+
+    // project
+    pipeline.push(
       {
         $project: {
           firstName: 1,
           lastName: 1,
+          fullName : { $concat : [ "$firstName", " ", "$lastName" ] },
           gender: 1,
           email: 1,
           createdAt: 1,
@@ -35,11 +71,49 @@ router.get('/', async function (req, res, next) {
           totalCreatedPost: { $size: "$createPost" }
         }
       }
-    ]);
-    const totalUsers = usersList.length;
+    );
 
+    pipeline.push({
+      $sort:{ [sortBy] : sortOrder }
+    })
+    console.log("pipeline =>", JSON.stringify(pipeline, null,2));
+
+    const usersList = await usersModel.aggregate(pipeline)
+    const totalUsers = usersList.length;
     console.log("usersList =>", usersList);
     console.log("totalUsers =>", totalUsers);
+    
+    // const usersList = await usersModel.aggregate([
+    //   {
+    //     $lookup: {
+    //       from: "savedposts",
+    //       localField: "_id",
+    //       foreignField: "savedBy",
+    //       as: "savedPost"
+    //     }
+    //   },
+    //   {
+    //     $lookup: {
+    //       from: "posts",
+    //       localField: "_id",
+    //       foreignField: "postBy",
+    //       as: "createPost"
+    //     }
+    //   },
+    //   {
+    //     $project: {
+    //       firstName: 1,
+    //       lastName: 1,
+    //       gender: 1,
+    //       email: 1,
+    //       createdAt: 1,
+    //       profilePicture: 1,
+    //       totalSavedPost: { $size: "$savedPost" },
+    //       totalCreatedPost: { $size: "$createPost" }
+    //     }
+    //   }
+    // ]);
+
 
     res.render("users/index", { title: "users", usersList: usersList, totalUsers: totalUsers })
   } catch (error) {
