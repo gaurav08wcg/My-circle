c09eb97820410ade61ba71cf8e624d42aff9185e
gaurav08wcg <gaurav.d@webcodegenie.com>
2023-04-27 19:18:26 +0530

    modify report

--- routes/report.js
+++ routes/report.js
@@ -1,118 +1,23 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const { usersModel } = require("../model/users");
+const { reportModel } = require("../model/report")
 const ObjectId = require("mongoose").Types.ObjectId;
-
+// const { usersModel } = require("../model/users");
+// const { postModel } = require("../model/post");
+// const { savedPostModel } = require("../model/saved-post");
 
 /* Repoart */
-router.get('/', async function (req, res, next) {
+router.get("/", async function (req, res, next) {
   try {
+    const usersReport = await reportModel.find().lean();
+    console.log("userReport", usersReport);
 
-    const usersDetails = await usersModel.aggregate([
-      {
-        $lookup: {
-          from: "posts",
-          let: { userId: "$_id" },
-          pipeline: [
-            {
-              $match: {
-                $expr: {
-                  $eq: ["$postBy", "$$userId"],
-                },
-              },
-            },
-          ],
-          as: "createdPost",
-        },
-      },
-
-      {
-        $lookup: {
-          from: "savedposts",
-          let: { userId: "$_id" },
-          pipeline: [
-            {
-              $match: {
-                $expr: {
-                  $eq: ["$$userId", "$savedBy"],
-                },
-              },
-            },
-          ],
-          as: "savedPost",
-        },
-      },
-      {
-        $project: {
-          firstName: 1,
-          lastName: 1,
-          fullName: { $concat: ["$firstName", " ", "$lastName"] },
-          profilePicture: {
-            name: 1,
-          },
-          totalCreatedPost: { $size: "$createdPost" },
-          totalSavedPost: { $size: "$savedPost" },
-        },
-      },
-    ]);
-
-    const savedPostByOthers = await usersModel.aggregate([
-      {
-        $lookup: {
-          from: "posts",
-          let: { userId: "$_id" },
-          pipeline: [
-            {
-              $match: {
-                $expr: {
-                  $and:
-                    [
-                      { $eq: ["$$userId", "$postBy"] },
-                      { "isArchived": false }
-                    ]
-                }
-              }
-            },
-            { $project: { "_id": 1 } },
-            {
-              $lookup: {
-                from: "savedposts",
-                localField: "_id",
-                foreignField: "postId",
-                as: "savedByOther"
-              }
-            },
-            { $project: { totalSavedPost: { $size: "$savedByOther" } } },
-            {
-              $group: {
-                _id: null,
-                totalSavedPost: { $sum: "$totalSavedPost" }
-              }
-            }
-          ],
-          as: "users_posts"
-        }
-      },
-      {
-        $project: {
-          total: "$users_posts.totalSavedPost"
-        }
-      }
-    ]);
-
-    console.log("usersDetails =>", JSON.stringify(usersDetails, null, 2));
-    console.log("savedPostByOthers =>", JSON.stringify(savedPostByOthers, null, 2));
-
-
-    res.render("report/index",
-      {
-        title: "report",
-        usersDetails: usersDetails,
-        savedPostByOthers: savedPostByOthers
-      }
-    )
+    res.render("report/index", {
+      title: "report",
+      usersReport:usersReport
+    });
   } catch (error) {
-    res.render("error", { message: error })
+    res.render("error", { message: error });
   }
 });
 
