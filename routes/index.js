10d13f1af9b49e77ba72b263907869567b7401b5
gaurav08wcg <gaurav.d@webcodegenie.com>
2023-04-28 15:16:47 +0530

    remove passport

--- routes/index.js
+++ routes/index.js
@@ -5,75 +5,81 @@ const { usersModel } = require("../model/users");
 const { postModel } = require("../model/post");
 const passport = require("passport");
 const { savedPostModel } = require("../model/saved-post");
-const LocalStrategy = require("passport-local").Strategy;
+// const LocalStrategy = require("passport-local").Strategy;
 const ObjectId = require("mongoose").Types.ObjectId;
+const auth = require("../auth");
+
+/* --------- passport authentication configuration ----------  */
+auth.localStrategyInitialization();
+auth.serializeUser();
+auth.deserializeUser();
 
 /* ====== Local strategy define ====== */
-passport.use(
-  new LocalStrategy(
-    {
-      usernameField: "email",
-      passwordField: "password",
-      passReqToCallback: true,
-    },
-    async function (req, email, password, done) {
-      console.log("----------- find user ---------");
-      usersModel
-        .findOne({
-          email: {
-            $regex: "^" + email + "$",
-            $options: "i",
-          },
-          password: md5(password),
-        })
-        .then((user) => {
-          console.log("-------- inside .then() ---------");
-          console.log("user =>", user);
-          // user not found
-          if (!user) {
-            console.log("-------- user not found ---------");
-            return done(null, false, {
-              message: "Please enter valid login details",
-            });
-          }
-          // user found
-          else {
-            console.log(
-              "======================/* user successfully founded */======================"
-            );
-            console.log("user =>", user);
-            return done(null, user);
-          }
-        })
-        // handle catch
-        .catch(function (err) {
-          console.log("err =>", err);
-          return done(null, false, {
-            message: "Please enter valid login details",
-          });
-        });
-    }
-  )
-);
+// passport.use(
+//   new LocalStrategy(
+//     {
+//       usernameField: "email",
+//       passwordField: "password",
+//       passReqToCallback: true,
+//     },
+//     async function (req, email, password, done) {
+//       console.log("----------- find user ---------");
+//       usersModel
+//         .findOne({
+//           email: {
+//             $regex: "^" + email + "$",
+//             $options: "i",
+//           },
+//           password: md5(password),
+//         })
+//         .then((user) => {
+//           console.log("-------- inside .then() ---------");
+//           console.log("user =>", user);
+//           // user not found
+//           if (!user) {
+//             console.log("-------- user not found ---------");
+//             return done(null, false, {
+//               message: "Please enter valid login details",
+//             });
+//           }
+//           // user found
+//           else {
+//             console.log(
+//               "======================/* user successfully founded */======================"
+//             );
+//             console.log("user =>", user);
+//             return done(null, user);
+//           }
+//         })
+//         // handle catch
+//         .catch(function (err) {
+//           console.log("err =>", err);
+//           return done(null, false, {
+//             message: "Please enter valid login details",
+//           });
+//         });
+//     }
+//   )
+// );
 
 /* =========== serialize user ===========*/
-passport.serializeUser(function (user, done) {
-  console.log("--------------serializeUser-------------");
-  // console.log("user =>", user)
-  done(null, user);
-});
+// passport.serializeUser(function (user, done) {
+//   console.log("--------------serializeUser-------------");
+//   // console.log("user =>", user)
+//   done(null, user);
+// });
 
 /* =========== deserialize user ===========*/
-passport.deserializeUser(function (user, done) {
-  try {
-    console.log("--------------deserializeUser--------------");
-    const userDetail = user;
-    // console.log("user =>", user)
-    done(null, userDetail);
-  } catch (error) {
-    console.log(error);
-  }
-});
+// passport.deserializeUser(function (user, done) {
+//   try {
+//     console.log("--------------deserializeUser--------------");
+//     const userDetail = user;
+//     // console.log("user =>", user)
+//     done(null, userDetail);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
 
 /* email validation */
 router.get("/validate/email", async (req, res, next) => {
