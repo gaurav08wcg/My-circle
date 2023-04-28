c48881164d262f7e442df3ce815e214520c3bbc5
gaurav08wcg <gaurav.d@webcodegenie.com>
2023-04-28 15:16:27 +0530

    add search event

--- public/javascripts/report.js
+++ public/javascripts/report.js
@@ -1,20 +1,19 @@
-const reportEvent = function (){
+const reportEvent = function () {
+  this.init = function () {
+    _this.searchUser();
+  };
 
-    this.init = function(){
-        _this.searchUser()
-    }
+  // search user
+  this.searchUser = function () {
+    $(document).on("keyup", "#search-user-field", function () {
+      const search = encodeURIComponent($(this).val().trim());
+      const url = `/report?search=${search}`;
+      $("div#user-table-card-body").load(`${url} div#user-table-card-body`, function () {
+        window.history.pushState(null, null, url);
+      });
+    });
+  };
 
-    // search user
-    this.searchUser = function(){
-        $(document).on("keyup", "#search-user-field", function(){
-            const search = encodeURIComponent($(this).val().trim());
-            const url = `/report?search=${search}`;
-            $(".table").load(`${url} .table`, function(){
-                window.history.pushState(null,null, url);
-            });
-        })
-    }
-
-    const _this = this;
-    _this.init();
-}
+  const _this = this;
+  _this.init();
+};
