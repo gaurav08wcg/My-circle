719e3d72502a235988dbd3e0dbfca9def55eb6d3
gaurav08wcg <gaurav.d@webcodegenie.com>
2023-04-28 19:17:58 +0530

    modify

--- public/javascripts/report.js
+++ public/javascripts/report.js
@@ -9,6 +9,7 @@ const reportEvent = function () {
       const search = encodeURIComponent($(this).val().trim());
       const url = `/report?search=${search}`;
       $("div#user-table-card-body").load(`${url} div#user-table-card-body`, function () {
+        $("#all-statistic").load(`${url} #all-statistic`)
         window.history.pushState(null, null, url);
       });
     });
