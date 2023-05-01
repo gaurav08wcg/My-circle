316ab3d256d70a40a0962fa415e20693e0d06811
gaurav08wcg <gaurav.d@webcodegenie.com>
2023-05-01 17:20:02 +0530

    modify db query

--- cron/cron.js
+++ cron/cron.js
@@ -11,8 +11,80 @@ const job = new CroneJob(
         console.log(" -------- Cron Job Running -------- ");
         
         // // 1. fetch all users
-        const users = await usersModel.find({},{ _id: 1, firstName: 1, lastName: 1});
-        // console.log("user =>", users);
+        // const users = await usersModel.find({},{ _id: 1, firstName: 1, lastName: 1});
+        // con sole.log("user =>", users);
+        const users = await usersModel.aggregate([
+          {
+            $lookup: {
+              from: "posts",
+              let: { userId: "$_id" },
+              pipeline: [
+                {
+                  $match: {
+                    $expr: {
+                      $eq: ["$$userId", "$postBy"],
+                    },
+                    isArchived: false,
+                  },
+                },
+                {
+                  $group: { _id: null, total: { $sum: 1 } },
+                },
+              ],
+              as: "createdPost",
+            },
+          },
+          {
+            $lookup: {
+              from: "savedposts",
+              let: { userId: "$_id" },
+              pipeline: [
+                {
+                  $match: {
+                    $expr: {
+                      $eq: ["$$userId", "$savedBy"],
+                    },
+                  },
+                },
+                {
+                  $group: { _id: null, total: { $sum: 1 } },
+                },
+              ],
+              as: "savedPostByMe",
+            },
+          },
+          {
+            $lookup: {
+              from: "savedposts",
+              let: { userId: "$_id" },
+              pipeline: [
+                {
+                  $match: {
+                    $expr: {
+                      $eq: ["$$userId", "$postBy"],
+                    },
+                  },
+                },
+                {
+                  $group: { _id: null, total: { $sum: 1 } },
+                },
+              ],
+              as: "savedPostByOther",
+            },
+          },
+          {
+            $project: {
+              _id: 1,
+              firstName: 1,
+              lastName: 1,
+              totalCreatedPost: { $arrayElemAt: ["$createdPost.total", 0] },
+              totalSavedPost: { $arrayElemAt: ["$savedPostByMe.total", 0] },
+              totalSavedByOther: {
+                $arrayElemAt: ["$savedPostByOther.total", 0],
+              },
+            },
+          },
+        ]);
 
 
         // 2. iterate all user 
@@ -21,9 +93,15 @@ const job = new CroneJob(
             const userDetails = {};
             userDetails["userId"] = user._id;
             userDetails["fullName"] = `${user.firstName} ${user.lastName}`;
-            userDetails["totalCreatedPost"] = await postModel.count({ postBy: user._id, isArchived : false });
-            userDetails["totalSavedPost"] = await savedPostModel.count({savedBy: user._id});
-            userDetails["totalSavedByOther"] = await savedPostModel.count({postBy: user._id});
+
+            !user.totalCreatedPost ? userDetails["totalCreatedPost"] = 0 : userDetails["totalCreatedPost"] = user?.totalCreatedPost;
+            !user.totalSavedPost ? userDetails["totalSavedPost"] =0 : userDetails["totalSavedPost"] = user?.totalSavedPost;
+            !user.totalSavedByOther ? userDetails["totalSavedByOther"] = 0 : userDetails["totalSavedByOther"] = user?.totalSavedByOther;
+            
+            /* total counts using separate query's */
+            // userDetails["totalCreatedPost"] = await postModel.count({ postBy: user._id, isArchived : false });
+            // userDetails["totalSavedPost"] = await savedPostModel.count({savedBy: user._id});
+            // userDetails["totalSavedByOther"] = await savedPostModel.count({postBy: user._id});
             // console.log("userDetails =>", userDetails); 
             
             // if new user add then insert other wise update 
